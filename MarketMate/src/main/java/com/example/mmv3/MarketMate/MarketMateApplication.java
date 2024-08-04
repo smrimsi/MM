06506bc41a943b604d5d50ec131a5e9e248bd291
Rish <smrimsi@gmail.com>
2024-08-04 22:05:38 +0530

    Initial commit for this project

---
+++ MarketMate/src/main/java/com/example/mmv3/MarketMate/MarketMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.mmv3.MarketMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketMateApplication.class, args);
+	}
+
+}
